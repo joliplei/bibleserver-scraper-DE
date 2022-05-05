d3f05ecb8abcc1719fd2d36ea6a3e21449f33150
Jotam <Jotam@users.noreply.github.com>
2022-05-05 16:47:26 +0200

    Update Book.java
    Add support for Spanish Bible translations

--- src/main/java/org/zephyrsoft/bibleserverscraper/model/Book.java
+++ src/main/java/org/zephyrsoft/bibleserverscraper/model/Book.java
@@ -5,72 +5,72 @@ import java.util.stream.Stream;
 
 public enum Book {
 
-	_01("1.Mose", "Genesis", 50),
-	_02("2.Mose", "Exodus", 40),
-	_03("3.Mose", "Leviticus", 27),
-	_04("4.Mose", "Numbers", 36),
-	_05("5.Mose", "Deuteronomy", 34),
-	_06("Josua", "Joshua", 24),
-	_07("Richter", "Judges", 21),
-	_08("Rut", "Ruth", 4),
-	_09("1.Samuel", "1 Samuel", 31),
-	_10("2.Samuel", "2 Samuel", 24),
-	_11("1.Könige", "1 Kings", 22),
-	_12("2.Könige", "2 Kings", 25),
-	_13("1.Chronik", "1 Chronicles", 29),
-	_14("2.Chronik", "2 Chronicles", 36),
-	_15("Esra", "Ezra", 10),
-	_16("Nehemia", "Nehemiah", 13),
-	_17("Esther", "Esther", 10),
-	_18("Hiob", "Job", 42),
-	_19("Psalmen", "Psalms", 150),
-	_20("Sprüche", "Proverbs", 31),
-	_21("Prediger", "Ecclesiastes", 12),
-	_22("Hoheslied", "Song of Solomon", 8),
-	_23("Jesaja", "Isaiah", 66),
-	_24("Jeremia", "Jeremiah", 52),
-	_25("Klagelieder", "Lamentations", 5),
-	_26("Hesekiel", "Ezekiel", 48),
-	_27("Daniel", "Daniel", 14),
-	_28("Hosea", "Hosea", 14),
-	_29("Joel", "Joel", 4),
-	_30("Amos", "Amos", 9),
-	_31("Obadja", "Obadiah", 1),
-	_32("Jona", "Jonah", 4),
-	_33("Micha", "Micah", 7),
-	_34("Nahum", "Nahum", 3),
-	_35("Habakuk", "Habbakuk", 3),
-	_36("Zefanja", "Zephaniah", 3),
-	_37("Haggai", "Haggai", 2),
-	_38("Sacharja", "Zechariah", 14),
-	_39("Maleachi", "Malachi", 3),
-	_40("Matthäus", "Matthew", 28),
-	_41("Markus", "Mark", 16),
-	_42("Lukas", "Luke", 24),
-	_43("Johannes", "John", 21),
-	_44("Apostelgeschichte", "Acts", 28),
-	_45("Römer", "Romans", 16),
-	_46("1.Korinther", "1 Corinthians", 16),
-	_47("2.Korinther", "2 Corinthians", 13),
-	_48("Galater", "Galatians", 6),
-	_49("Epheser", "Ephesians", 6),
-	_50("Philipper", "Philippians", 4),
-	_51("Kolosser", "Colossians", 4),
-	_52("1.Thessalonicher", "1 Thessalonians", 5),
-	_53("2.Thessalonicher", "2 Thessalonians", 3),
-	_54("1.Timotheus", "1 Timothy", 6),
-	_55("2.Timotheus", "2 Timothy", 4),
-	_56("Titus", "Titus", 3),
-	_57("Philemon", "Philemon", 1),
-	_58("Hebräer", "Hebrews", 13),
-	_59("Jakobus", "James", 5),
-	_60("1.Petrus", "1 Peter", 5),
-	_61("2.Petrus", "2 Peter", 3),
-	_62("1.Johannes", "1 John", 5),
-	_63("2.Johannes", "2 John", 1),
-	_64("3.Johannes", "3 John", 1),
-	_65("Judas", "Jude", 1),
-	_66("Offenbarung", "Revelation", 22);
+	_01("1.Mose", "Genesis", "Génesis", 50),
+	_02("2.Mose", "Exodus", "Éxodo", 40),
+	_03("3.Mose", "Leviticus", "Levitico", 27),
+	_04("4.Mose", "Numbers", "Números", 36),
+	_05("5.Mose", "Deuteronomy", "Deuteronomio", 34),
+	_06("Josua", "Joshua", "Josué", 24),
+	_07("Richter", "Judges", "Jueces", 21),
+	_08("Rut", "Ruth", "Rut", 4),
+	_09("1.Samuel", "1 Samuel", "1 Samuel", 31),
+	_10("2.Samuel", "2 Samuel", "2 Samuel", 24),
+	_11("1.Könige", "1 Kings", "1 Reyes", 22),
+	_12("2.Könige", "2 Kings", "2 Reyes", 25),
+	_13("1.Chronik", "1 Chronicles", "1 Crónicas", 29),
+	_14("2.Chronik", "2 Chronicles", "2 Crónicas", 36),
+	_15("Esra", "Ezra", "Esdras", 10),
+	_16("Nehemia", "Nehemiah", "Nehemías", 13),
+	_17("Esther", "Esther", "Ester", 10),
+	_18("Hiob", "Job", "Job", 42),
+	_19("Psalmen", "Psalms", "Salmos", 150),
+	_20("Sprüche", "Proverbs", "Proverbios", 31),
+	_21("Prediger", "Ecclesiastes", "Eclesiastés", 12),
+	_22("Hoheslied", "Song of Solomon", "Cantares", 8),
+	_23("Jesaja", "Isaiah", "Isaías", 66),
+	_24("Jeremia", "Jeremiah", "Jeremías", 52),
+	_25("Klagelieder", "Lamentations", "Lamentaciones", 5),
+	_26("Hesekiel", "Ezekiel", "Ezequiel", 48),
+	_27("Daniel", "Daniel", "Daniel", 14),
+	_28("Hosea", "Hosea", "Oseas", 14),
+	_29("Joel", "Joel", "Joel", 4),
+	_30("Amos", "Amos", "Amós", 9),
+	_31("Obadja", "Obadiah", "Abdías", 1),
+	_32("Jona", "Jonah", "Jonás", 4),
+	_33("Micha", "Micah", "Miqueas", 7),
+	_34("Nahum", "Nahum", "Nahum", 3),
+	_35("Habakuk", "Habbakuk", "Habacuc", 3),
+	_36("Zefanja", "Zephaniah", "Sofonías", 3),
+	_37("Haggai", "Haggai", "Hageo", 2),
+	_38("Sacharja", "Zechariah", "Zacarías", 14),
+	_39("Maleachi", "Malachi", "Malaquías", 3),
+	_40("Matthäus", "Matthew", "Mateo", 28),
+	_41("Markus", "Mark", "Marcos", 16),
+	_42("Lukas", "Luke", "Lucas", 24),
+	_43("Johannes", "John", "Juan", 21),
+	_44("Apostelgeschichte", "Acts", "Hechos", 28),
+	_45("Römer", "Romans", "Romanos", 16),
+	_46("1.Korinther", "1 Corinthians", "1 Corintios", 16),
+	_47("2.Korinther", "2 Corinthians", "2 Corintios", 13),
+	_48("Galater", "Galatians", "Gálatas", 6),
+	_49("Epheser", "Ephesians", "Efesios", 6),
+	_50("Philipper", "Philippians", "Filipenses", 4),
+	_51("Kolosser", "Colossians", "Colosenses", 4),
+	_52("1.Thessalonicher", "1 Thessalonians", "1 Tesalonicenses", 5),
+	_53("2.Thessalonicher", "2 Thessalonians", "2 Tesalonicenses", 3),
+	_54("1.Timotheus", "1 Timothy", "1 Timoteo", 6),
+	_55("2.Timotheus", "2 Timothy", "2 Timoteo", 4),
+	_56("Titus", "Titus", "Tito", 3),
+	_57("Philemon", "Philemon", "Filemón", 1),
+	_58("Hebräer", "Hebrews", "Hebreos", 13),
+	_59("Jakobus", "James", "Santiago", 5),
+	_60("1.Petrus", "1 Peter", "1 Pedro", 5),
+	_61("2.Petrus", "2 Peter", "2 Pedro", 3),
+	_62("1.Johannes", "1 John", "1 Juan", 5),
+	_63("2.Johannes", "2 John", "2 Juan", 1),
+	_64("3.Johannes", "3 John", "3 Juan", 1),
+	_65("Judas", "Jude", "Judas", 1),
+	_66("Offenbarung", "Revelation", "Apocalipsis", 22);
 
 	public static Stream<Book> books() {
 		return Stream.of(values());
@@ -78,11 +78,13 @@ public enum Book {
 
 	private String nameGerman;
 	private String nameEnglish;
+	private String nameSpanish;
 	private int chapters;
 
-	private Book(String nameGerman, String nameEnglish, int chapters) {
+	private Book(String nameGerman, String nameEnglish, String nameSpanish, int chapters) {
 		this.nameGerman = nameGerman;
 		this.nameEnglish = nameEnglish;
+		this.nameSpanish = nameSpanish;
 		this.chapters = chapters;
 	}
 
@@ -105,10 +107,10 @@ public enum Book {
 
 	public Stream<BookChapter> bookChapters() {
 		if (chapters == 1) {
-			return Stream.of(new BookChapter(this, 1, nameGerman, nameEnglish));
+			return Stream.of(new BookChapter(this, 1, nameGerman, nameEnglish, nameSpanish));
 		} else {
 			return IntStream.range(1, chapters + 1)
-				.mapToObj(chapter -> new BookChapter(this, chapter, nameGerman + chapter, nameEnglish + chapter));
+				.mapToObj(chapter -> new BookChapter(this, chapter, nameGerman + chapter, nameEnglish + chapter, nameSpanish + chapter));
 		}
 	}
 
